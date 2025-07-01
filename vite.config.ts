67615612f6f752b2f8e03795f9d027a8971938ea
Amritesh Kumar <amriteshkr0815@gmail.com>
2025-07-01 17:06:32 +0530

    fixed final

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [react(), visualizer({ open: true })],
-  base: './', // Ensures assets are correctly resolved in production
+  base: '/dashboard/', // Updated base to match app served under /dashboard path
   build: {
     outDir: 'dist', // Vercel default output directory
     sourcemap: false, // Set to true if you want source maps for debugging
