e8bf9b33abf61685a1ff04f854d6b2f799f6a477
Amritesh Kumar <amriteshkr0815@gmail.com>
2025-07-01 15:21:54 +0530

    fixed nugs

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   build: {
-    outDir: 'dist', // ✅ Vercel expects this
-    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit to 1000 kB
     rollupOptions: {
       output: {
         manualChunks(id) {
@@ -18,12 +13,8 @@ export default defineConfig({
             }
             return 'vendor';
           }
-        },
-      },
-    },
-  },
-  server: {
-    host: '0.0.0.0', // ✅ good for Render and local testing
-    port: 5173,
-  },
+        }
+      }
+    }
+  }
 });
