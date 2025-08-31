428b80f4f48b7423e0e077f3028d3031a961eecf
Amritesh Kumar <amriteshkr0815@gmail.com>
2025-08-31 22:09:26 +0530

    Sign up fixed

--- src/services/authServices/authApi.js
+++ src/services/authServices/authApi.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const baseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const API_BASE = `${baseUrl}/api`;
 export const getCurrentUser = async () => {
   try {
     const response = await axios.get(`${API_BASE}/auth/me`, {
